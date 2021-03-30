fba7d7f51d15ff29b86553e93ab68b1463132d99
DaniaAbughoush <ms1dodo@gmail.com>
2021-03-30 19:24:27 +0300

    adding local storeage

--- Js/app.js
+++ Js/app.js
@@ -1,4 +1,8 @@
 // global
+// let stringObject=localStorage.getItem('storeProduct');
+// if(!strobject){
+//   return false
+// }
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
@@ -10,9 +14,9 @@ let leftIndex;
 let middleIndex;
 let rightIndex;
 let rounds =25;
-let shownImages = [];
-let views=[];
+let shownImages=[];
 let votes=[];
+let views=[];
 
 //products name:
 let names = [
@@ -30,36 +34,73 @@ let names = [
   'pet-sweep',
   'scissors',
   'shark',
-  'sweep',
   'tauntaun',
   'unicorn',
-  'usb',
   'water-can',
   'wine-glass',
-];
+  'usb',
+  'sweep',
 
+];
+//bring images and section from html
 const rightImage = document.getElementById('right');
 const leftImage = document.getElementById('left');
 const middleImage = document.getElementById('middle');
 const imageSection = document.getElementById('product');
 const finalResult = document.getElementById('finalResult');
-
+//constructer
 function Products(name) {
   this.name = name;
-  this.path = `./images/${name}.jpg`;
-  this.vote = [];
-  this.view = [];
+  this.path=`./images/${name}.jpg`;
+  this.vote = 0;
+  this.view = 0;
   Products.all.push(this);
+
+  // gettingItem();
+  // settingIteam();
 }
 Products.all = [];
+// gettingItem();
+// settingIteam();
+// gettingItem();
 
+//bulding object
 for (let i = 0; i < names.length; i++) {
   new Products(names[i]);
 }
 
 
+function randomIndex(){
+  leftIndex = randomNumber(0, Products.all.length - 1);
+  rightIndex = randomNumber(0, Products.all.length - 1);
+  middleIndex = randomNumber(0, Products.all.length - 1);
+}
 
-function renderImage(){
+
+
+
+
+
+function render() {
+
+  do{randomIndex();
+  }
+  while(leftIndex === middleIndex || leftIndex === rightIndex ||rightIndex === middleIndex);
+
+
+  renderImages();
+
+}
+// renderImages();
+
+// if(shownImages.includes(leftIndex)||shownImages.includes(middleIndex)||shownImages.includes(rightIndex)){
+//   randomIndex();
+// }
+
+
+render();
+
+function renderImages(){
   leftImage.src = Products.all[leftIndex].path;
   leftImage.alt = Products.all[leftIndex].name;
   leftImage.title = Products.all[leftIndex].name;
@@ -79,36 +120,16 @@ function renderImage(){
 
 
 
-function render() {
-  do {
-    leftIndex = randomNumber(0, Products.all.length - 1);
-  }while (leftIndex === middleIndex || leftIndex === rightIndex || checkAvailability(leftIndex));
-  do{
-    rightIndex = randomNumber(0, Products.all.length - 1);
-  } while ( leftIndex === rightIndex || rightIndex === middleIndex|| checkAvailability(rightIndex));
-  do {middleIndex = randomNumber(0, Products.all.length - 1);
-  } while (leftIndex === middleIndex || rightIndex === middleIndex || checkAvailability(middleIndex));
+// function checkAvailability(selectedImageName) {
+//   for (let i = 0; i < shownImages.length; i++) {
+//     if (shownImages[i] === selectedImageName) {
+//       return true;
+//     }
 
-  shownImages. push(leftIndex);
-  shownImages. push(rightIndex);
-  shownImages. push(middleIndex);
-  // console.log(shownImages);
-  renderImage();
-}
-render();
+//     return false;
 
-
-
-function checkAvailability(selectedImageName) {
-  for (let i = 0; i < shownImages.length; i++) {
-    if (shownImages[i] === selectedImageName) {
-      return true;
-    }
-
-    return false;
-
-  }
-}
+//   }
+// }
 
 // } console.log(checkAvailability());
 
@@ -120,20 +141,24 @@ let totalVote = 0;
 function handelClick(event) {
   event.preventDefault();
   if (event.target.id !== 'product') {
+
     if (event.target.id === 'right') {
       totalVote++;
       Products.all[rightIndex].vote++;
-      votes.push( Products.all[rightIndex].vote++);
+      // votes.push( Products.all[rightIndex].vote++);
     } else if (event.target.id === 'left') {
       totalVote++;
       Products.all[leftIndex].vote++;
-      votes.push( Products.all[leftIndex].vote++);
+      // votes.push( Products.all[leftIndex].vote++);
     } else {
       totalVote++;
       Products.all[middleIndex].vote++;
-      votes.push( Products.all[middleIndex].vote++);
+      // votes.push( Products.all[middleIndex].vote++);
+
 
     }
+    //moving shownimages here to not consider first render in counting
+
 
     // for(let i=0;i<names.length;){
     //   votes.push( Products.all[i].vote);
@@ -157,8 +182,9 @@ function handelClick(event) {
 
 }
 
-function ProductList() {
 
+
+function ProductList() {
   const container = document.getElementById('buttonList');
   const ulEl = document.createElement('ul');
   container.appendChild(ulEl);
@@ -170,13 +196,21 @@ function ProductList() {
     ulEl.appendChild(liEl);
     liEl.textContent = `${Products.all[i].name} had ${Products.all[i].vote} votes, and was seen ${Products.all[i].view} times.`;
     votes.push(Products.all[i].vote);
-    resultChart();
+    views.push(Products.all[i].view);
 
-  }
-
-  finalResult.removeEventListener('click', ProductList);
+  shownImages. push(leftIndex);
+  shownImages. push(rightIndex);
+  shownImages. push(middleIndex);
+  // console.log(shownImages);
+  resultChart();
+  // gettingItem();
+  settingIteam();
+  // gettingItem();
 
 }
+
+}
+// settingIteam();
 
 // console.log(votes);
 
@@ -193,7 +227,7 @@ function resultChart(){
         label: 'views',
         backgroundColor: 'rgb(255, 99, 132)',
         borderColor: 'rgb(255, 99, 132)',
-        data: shownImages
+        data:views
       },
       {
         label: 'votes',
@@ -208,3 +242,72 @@ function resultChart(){
     options: {}
   });
 }
+//local storge
+function settingIteam(){
+  let data=JSON.stringify(Products.all);
+  localStorage.setItem('storeProduct',data);
+  // console.log(data);
+}
+
+function gettingItem(){
+  let stringObject=localStorage.getItem('storeProduct');
+  console.log(stringObject);
+
+  let normal=JSON.parse(stringObject);
+  if(normal !== null){
+    Products.all=normal;
+
+    // lastResults();
+
+
+  }
+  // ProductList();
+//   result();
+//
+}
+gettingItem();
+
+// function lastResults(){
+//   let namesArr = [];
+//   let viewsArr = [];
+
+//   for (let j = 0; j < Products.all.length; j++){
+//     let namesResults = Products.all[j].name;
+//     namesArr.push(namesResults);
+
+//     let viewResults = Products.all[j].views;
+//     viewsArr.push(viewResults);
+//   }}
+// function resultstore(){
+//   for (let i = 0; i < names.length; i++){
+//     `${Products.all[i].name} had ${Products.all[i].vote} votes, and was seen ${Products.all[i].view} times.`
+//     votes.push(Products.all[i].vote);
+//     views.push(Products.all[i].view);
+
+//   }
+
+//  function traceYourCode(){
+//   const container = document.getElementById('trace');
+//   const ul1El = document.createElement('ul');
+//   container.appendChild(ul1El);
+   
+//   for (let i = 0; i < names.length; i++) {
+//     // votes.push(Products.all[i].vote)
+//     const li1El = document.createElement('li');
+//     ul1El.appendChild(li1El);
+//     li1El.textContent = `${Products.all[i].name} had ${Products.all[i].vote} votes, and was seen ${Products.all[i].view} times.`;
+   
+
+//   // shownImages. push(leftIndex);
+//   // shownImages. push(rightIndex);
+//   // shownImages. push(middleIndex);
+//   // console.log(shownImages);
+//   // resultChart();
+//   // gettingItem();
+//   // settingIteam();
+//   // gettingItem();
+
+
+
+// }
+//  }
